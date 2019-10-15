a83f876ce9251fbd982b66f7f957829955c854a4
Roman Gafiyatullin <r.gafiyatullin@me.com>
2019-10-15 14:15:05 +0300

    0.3.0: mgmt-api — Access-Control-Allow-Origin and Access-Control-Allow-Credentials headers

--- src/http_server/mgmt_api.rs
+++ src/http_server/mgmt_api.rs
@@ -1,15 +1,24 @@
 use std::sync::Arc;
 
 use warp::Filter;
+use warp::http::header::HeaderValue;
 
 use crate::storage::StorageApi;
 
 pub fn routes(
     storage_api: Arc<StorageApi>,
 ) -> impl warp::Filter<Extract = (impl warp::Reply,), Error = warp::Rejection> + Clone {
-    let index = warp::path::end().and(warp::get2()).map({
-        let storage_api = Arc::clone(&storage_api);
-        move || warp::reply::json({ &storage_api.ids() })
+    let index = warp::path::end().and(warp::get2())
+        .map({
+            let storage_api = Arc::clone(&storage_api);
+            move || {
+                let reply = warp::reply::json({ &storage_api.ids() });
+
+                let reply = warp::reply::with_header(reply, "Access-Control-Allow-Origin", HeaderValue::from_static("*"));
+                let reply = warp::reply::with_header(reply, "Access-Control-Allow-Credentials", HeaderValue::from_static("true"));
+
+                reply
+            }
     });
 
     let get_by_id = path!(u64).and(warp::get2()).map({
@@ -24,7 +33,14 @@ pub fn routes(
 
     let all = path!("dump-state").and(warp::get2()).map({
         let storage_api = Arc::clone(&storage_api);
-        move || warp::reply::json(&storage_api.get_data())
+        move || {
+            let reply = warp::reply::json(&storage_api.get_data());
+
+            let reply = warp::reply::with_header(reply, "Access-Control-Allow-Origin", HeaderValue::from_static("*"));
+            let reply = warp::reply::with_header(reply, "Access-Control-Allow-Credentials", HeaderValue::from_static("true"));
+
+            reply
+        }
     });
 
     all.or(index).or(get_by_id)
